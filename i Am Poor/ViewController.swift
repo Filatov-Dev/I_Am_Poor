2bf9b99c6b7549167848ed3b9d432ff7bd4c293b
Юрий Филатов <urijfilatov@MacBook-Pro-Urij.local>
2021-06-07 19:26:33 +0300

    I Am Poor

---
+++ i Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  i Am Poor
+//
+//  Created by Юрий Филатов on 07.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
